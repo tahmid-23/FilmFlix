2fa5eef28b03e754512c1881dd74f35b1730ebe0
Johan Mathew <johanmathew.george@gmail.com>
2023-09-23 18:49:55 -0400

    Change background color

--- frontend/src/pages/Friends.tsx
+++ frontend/src/pages/Friends.tsx
@@ -1,15 +1,36 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Button} from 'react-bootstrap';
 import '../main.css';
 import MainNav from '../components/MainNav';
-import FeedBox from '../components/FeedBox';
 
+import {AiOutlineUserAdd} from 'react-icons/ai'
 // Add typing for props when necessary
 export default function Friends({ props }: any) {
   return (
     <>
       <Container fluid>
         <MainNav page="Friends"></MainNav>
-        <FeedBox />
+        <Container>
+
+          <Row style={{marginTop: "10vw"}} className="d-flex align-items-center" >
+            <Col xs lg = {5}>
+              <h1 style={{fontSize:70, color:"white"}}>
+                Friends
+              </h1>
+            </Col>
+
+            <Col xs lg = {1}>
+            <Button>
+              <AiOutlineUserAdd backgroundColor="black"></AiOutlineUserAdd>
+            </Button>
+            </Col>
+
+            <Col xs lg = {6}>
+            </Col>
+            
+
+          </Row>
+
+        </Container>
       </Container>
     </>
   );
