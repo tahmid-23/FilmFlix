6cd8c0ca65bd51d93a06717211aad1449e44f819
Johan Mathew <johanmathew.george@gmail.com>
2023-09-24 00:03:28 -0400

    Adding API access
    Testing the API access to The Movies Database

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,4 +1,7 @@
 export default function SearchBar() {
+
+  testApi(); 
+  
   return (
     <div className="input-group rounded">
       <input
@@ -13,3 +16,18 @@ export default function SearchBar() {
     </div>
   );
 }
+
+async function testApi() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMWMxMDU2Y2UyYmMwMmRjOGU0OWY5NTQzYjU0MjRkMCIsInN1YiI6IjY1MGZiMDE5MDljMjRjMDBlMmVhOTMyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4ZlD7cWUhr2ey7D9_W98MHaJqwWUdc-4JPp_Ey0L55U'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/search/movie?query=Mulan&include_adult=false&language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+};
