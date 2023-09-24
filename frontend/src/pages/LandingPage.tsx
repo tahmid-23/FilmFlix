4f77dfc9322c3c6b3c85d35b05888d5f0b3177e8
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2023-09-23 22:28:17 -0400

    navigate to feed if already logged in

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,8 +1,16 @@
 import { Container } from 'react-bootstrap';
 import MainNav from '../components/MainNav';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 
 // Add typing for props when necessary
 export default function LandingPage({ props }: any) {
+  const { isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+  if (isAuthenticated) {
+    navigate('/feed');
+  }
+
   return (
     <>
       <Container
