cdd5b84272bbe67d7692faac7270b6c83fb068dc
Steven Ha <stevenha75@gmail.com>
2023-09-23 21:57:41 -0400

    FeedBox icon update

--- frontend/src/components/FeedBox.tsx
+++ frontend/src/components/FeedBox.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 import { Card } from 'react-bootstrap';
+import { Stack } from 'react-bootstrap';
+import { BsPersonCircle } from 'react-icons/bs';
 
 interface FeedBoxProps {
   title: string;
@@ -10,13 +12,23 @@ interface FeedBoxProps {
 export default function FeedBox({ title, subtitle, info }: FeedBoxProps) {
   return (
     <>
-      <Card style={{ borderRadius: '15px', marginTop: '10px' }}>
-        <div style={{ padding: '10px' }}>
-          <Card.Title>{title}</Card.Title>
-          <Card.Subtitle>{subtitle}</Card.Subtitle>
-          <Card.Text>{info}</Card.Text>
+      <Stack gap={3} direction="horizontal" style={{ padding: '2vh 2vw' }}>
+        <div className="icon-container">
+          <BsPersonCircle
+            style={{ color: 'white', width: '100px', height: '100px' }}
+            className="profile-icon"
+          />
         </div>
-      </Card>
+        <Card
+          style={{ borderRadius: '15px', marginTop: '10px', width: '100%' }}
+        >
+          <div style={{ padding: '10px' }}>
+            <Card.Title>{title}</Card.Title>
+            <Card.Subtitle>{subtitle}</Card.Subtitle>
+            <Card.Text>{info}</Card.Text>
+          </div>
+        </Card>
+      </Stack>
     </>
   );
 }
