881d134c101aca5a985d4825071e58ac3faf98cc
Steven Ha <stevenha75@gmail.com>
2023-09-23 22:51:16 -0400

    Search navTab

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -17,8 +17,9 @@ function BasicLink({ tab }: { tab: string }) {
 }
 
 const navTabs: Record<string, string[]> = {
-  Friends: ["Profile", 'Search', 'Feed', 'Logout'],
-  Feed: ["Profile", 'Search', 'Friends', 'Logout']
+  Friends: ['Profile', 'Search', 'Feed', 'Logout'],
+  Feed: ['Profile', 'Search', 'Friends', 'Logout'],
+  Search: ['Profile', 'Feed', 'Friends', 'Logout']
 };
 
 export default function MainNav({ page }: Page) {
@@ -36,7 +37,7 @@ export default function MainNav({ page }: Page) {
         Sign Up
       </Nav.Link>,
       <Nav.Link
-        style={{ paddingRight: 15, paddingLeft: 15}}
+        style={{ paddingRight: 15, paddingLeft: 15 }}
         onClick={() => loginWithRedirect()}
       >
         Log In
@@ -44,17 +45,20 @@ export default function MainNav({ page }: Page) {
     ];
   } else {
     navLinks = navTabs[page].map((tab: string) => {
-
-      console.log(tab); 
-      if(tab === 'Logout') {
-        return (<Nav.Link style={{ paddingRight: 15, paddingLeft: 15 }} onClick={() => logout()}>
-        Logout
-        </Nav.Link>); 
-      }
-      else if(tab === 'Profile') {
+      console.log(tab);
+      if (tab === 'Logout') {
         return (
           <Nav.Link
-            style={{ paddingRight: 15, paddingLeft:15 }}
+            style={{ paddingRight: 15, paddingLeft: 15 }}
+            onClick={() => logout()}
+          >
+            Logout
+          </Nav.Link>
+        );
+      } else if (tab === 'Profile') {
+        return (
+          <Nav.Link
+            style={{ paddingRight: 15, paddingLeft: 15 }}
             onClick={() => {
               if (isAuthenticated) {
                 getAccessTokenSilently()
@@ -65,9 +69,9 @@ export default function MainNav({ page }: Page) {
               }
             }}
           >
-          Profile
+            Profile
           </Nav.Link>
-        ); 
+        );
       }
       return <BasicLink tab={tab} />;
     });
